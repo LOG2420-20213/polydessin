3e19be843c39edfca98bcb7de454880bf4c10136
kegaub <kevin.gauthier.22.06@gmail.com>
2021-10-28 09:30:10 -0400

    Initial commit

---
+++ src/canvas-loader.js
@@ -0,0 +1,9 @@
+export default function loadCanvas() {
+  const baseCanvas = document.getElementById("base-canvas");
+  const context = baseCanvas ? baseCanvas.getContext("2d") : null;
+
+  return {
+    canvas: baseCanvas,
+    context: context,
+  };
+}
